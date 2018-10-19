ff37e505f7300d6808079a3b2f43bbecbefa9e53
mengyang <mengeryang163@gmail.com>
2018-10-19 08:30:52 +0800

    init
    Signed-off-by: mengyang <mengeryang163@gmail.com>

---
+++ js/gallery.js
@@ -0,0 +1,9 @@
+function showPic(whichpic) {
+    var source = whichpic.getAttribute('href');
+    var placeholder = document.getElementById('placeholder');
+    var text = whichpic.getAttribute('title');
+    var description = document.getElementById('description');
+    placeholder.setAttribute('src', source);
+    description.firstChild.nodeValue = text;
+
+}
